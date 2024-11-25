9b336e6598e8e8cb2034fd7db2b4d153ea66b473
VanessaLops <vanessashirlene_estrela@hotmail.com>
2024-11-25 16:18:08 -0300

    feat: atualizando url rota do backend para que get e delete funcione

--- src/services/api/userService.ts
+++ src/services/api/userService.ts
@@ -2,47 +2,18 @@ import axios from 'axios';
 import { User } from '../../models/createUserDTO';
 
 
-const API_URL = 'https://boasorte.teddybackoffice.com.br';
-
-const corsProxy = 'https://thingproxy.freeboard.io/fetch/';
-
-
-const isProduction = import.meta.env.MODE === 'production';
+const API_URL = 'https://backend-micro-frontend-8288.vercel.app';
 
 
 export const getUsers = async (page: number, limit: number) => {
-  if (isProduction) {
+  try {
+     const response = await axios.get(`${API_URL}/api?page=${page}&limit=${limit}`);
 
-    try {
-      const response = await fetch('/clients.json');
-      if (!response.ok) {
-        throw new Error('Erro ao carregar o arquivo JSON');
-      }
-      const data = await response.json();
-
-   
-      const startIndex = (page - 1) * limit;
-      const endIndex = startIndex + limit;
-      const paginatedClients = data.clients.slice(startIndex, endIndex);
-
-      return {
-        clients: paginatedClients,
-        currentPage: page,
-        totalPages: Math.ceil(data.clients.length / limit),
-      };
-    } catch (error) {
-      console.error('Erro ao buscar os dados locais em produção', error);
-      throw error;
-    }
-  } else {
- 
-    try {
-      const response = await axios.get(corsProxy + `${API_URL}/users?page=${page}&limit=${limit}`);
-      return response.data; 
-    } catch (error) {
-      console.error('Erro ao buscar os dados da API', error);
-      throw error;
-    }
+    console.log(response)
+    return response.data; 
+  } catch (error) {
+    console.error('Erro ao buscar os dados da API', error);
+    throw error;
   }
 };
 
@@ -50,8 +21,9 @@ export const getUsers = async (page: number, limit: number) => {
 
 
 export const createUser = async (userData: User) => {
+  console.log(userData)
   try {
-    const response = await axios.post(corsProxy + `${API_URL}/users`, userData);
+    const response = await axios.post(`${API_URL}/api`, userData);
     return response.data;
   } catch (error) {
     console.error('Erro ao criar o usuário', error);
@@ -61,7 +33,7 @@ export const createUser = async (userData: User) => {
 
 export const updateUser = async (userId: number, userData: Partial<User>) => {
   try {
-    const response = await axios.patch(corsProxy + `${API_URL}/users/${userId}`, userData);
+    const response = await axios.patch(`${API_URL}/api/${userId}`, userData);
 
     return response.data;
   } catch (error) {
@@ -74,7 +46,7 @@ export const updateUser = async (userId: number, userData: Partial<User>) => {
 
 export const deleteUser = async (userId: number): Promise<void> => {
   try {
-    await axios.delete(corsProxy + `${API_URL}/users/${userId}`);
+    await axios.delete(`${API_URL}/api/${userId}`);
   } catch (error) {
     console.error('Erro ao excluir o usuário', error);
     throw error;
